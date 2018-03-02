8a49ea3187afe6b85e3bb9c1fa92f54eee1cf4b7
Epic Jefferson <epicjefferson@gmail.com>
2018-03-02 11:08:08 -0500

    Added 09_Touch_Sensor

---
+++ _09_Touch_Sensor/_09_Touch_Sensor.ino
@@ -0,0 +1,30 @@
+/*
+Arduino     Touch Sensor
+  5V         VCC
+  GND        GND
+  D2         SIG
+*/
+
+int TouchSensor = 2; //connected to Digital pin D3
+int led = 3;
+
+void setup(){
+  Serial.begin(9600); // Communication speed
+  pinMode(led, OUTPUT);
+  pinMode(TouchSensor, INPUT);
+}
+
+void loop(){
+  if(digitalRead(TouchSensor)==HIGH)       //Read Touch sensor signal
+   { 
+    digitalWrite(led, HIGH);   // if Touch sensor is HIGH, then turn on
+    Serial.println("Led ON");
+   }
+  else
+   {
+    digitalWrite(led, LOW);    // if Touch sensor is LOW, then turn off the led
+    Serial.println("Led OFF");
+   }
+  delay(50); // Slow down the output for easier reading
+}
+
